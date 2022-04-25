1882060015b36a5df2699a98b29e8d2dd7730ae6
OgataRyuji <codetrain.ryuji.ogata@gmail.com>
2022-04-25 15:57:19 +0900

    practice Batch

---
+++ src/app/Console/Commands/TestBatch.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\User;
+
+class TestBatch extends Command
+{
+	/**
+	 * The name and signature of the console command.
+	 * artisanコマンドで呼び出す時のコマンド名を定義する
+	 * @var string
+	 */
+	protected $signature = 'batch:test';
+
+	/**
+	 * The console command description.
+	 * artisanコマンド一覧の出力時に表示される説明文、必須ではないが設定推奨
+	 * @var string
+	 */
+	protected $description = 'お試しのバッチ処理';
+
+	/**
+	 * Create a new command instance.
+	 * 
+	 * @return void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	/**
+	 * Execute the console command.
+	 *  実際の処理をこのメソッド内に記述する
+	 * @return int
+	 */
+	public function handle()
+	{
+		echo User::all();
+		for ($i=0; $i < 4; $i++) { 
+			echo "Hello!";
+		}
+	}
+}
--- src/app/Console/Kernel.php
+++ src/app/Console/Kernel.php
@@ -4,29 +4,48 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Console\Commands\TestBatch;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
-    {
-        // $schedule->command('inspire')->hourly();
-    }
+	/**
+	 * The Artisan command provided by your application.
+	 * ここにartisanコマンドクラスを記述する
+	 *
+	 * @return array
+	 */
+	protected $commands = [
+		TestBatch::class,
+	];
 
-    /**
-     * Register the commands for the application.
-     *
-     * @return void
-     */
-    protected function commands()
-    {
-        $this->load(__DIR__.'/Commands');
+	/**
+	 * Define the application's command schedule.
+	 * artisanコマンドの実行スケジュールを記述する
+	 * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+	 * @return void
+	 */
+	protected function schedule(Schedule $schedule)
+	{
+			// $schedule->command('inspire')->hourly();
+			//クロージャに直接処理を書くことも可能
+			$schedule->call(function(){
+				echo 'hello!\n';
+			});
 
-        require base_path('routes/console.php');
-    }
+			//batch::testを毎時実行し、出力先を$filePathに指定する
+			$filePath = '/src/storage/logs/laravel.log';
+			$schedule->command('batch:test')->sendOutputTo($filePath)->hourly();
+	}
+
+	/**
+	 * Register the commands for the application.
+	 *
+	 * @return void
+	 */
+	protected function commands()
+	{
+			$this->load(__DIR__.'/Commands');
+
+			require base_path('routes/console.php');
+	}
 }
